60d12cd8b59bf3d6655345310922325963c64d61
agapay-conqueror <agapayconqueror@gmail.com>
2020-11-30 16:51:56 +0800

    adding 3 layouts (topics,map,videos) button

---
+++ Agapay/app/src/main/java/com/example/agapay/PhilippineMap.java
@@ -0,0 +1,14 @@
+package com.example.agapay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhilippineMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_philippine_map);
+    }
+}
