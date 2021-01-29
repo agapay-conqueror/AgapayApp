845d9dce26a9e077626087e691da7550acda7e18
agapay-conqueror <agapayconqueror@gmail.com>
2021-01-29 19:28:48 +0800

    adding info

--- Agapay/app/src/main/java/com/example/agapay/Philippines.java
+++ Agapay/app/src/main/java/com/example/agapay/Philippines.java
@@ -2,7 +2,9 @@ package com.example.agapay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.github.chrisbanes.photoview.PhotoView;
 
@@ -15,7 +17,18 @@ public class Philippines extends AppCompatActivity {
 
         PhotoView philippines = findViewById(R.id.philippines);
         PhotoView philippines1 = findViewById(R.id.philippines1);
+        PhotoView flag = findViewById(R.id.flag);
+        PhotoView flag1 = findViewById(R.id.flag1);
+        PhotoView coat = findViewById(R.id.coat);
 
+        philippines.setImageResource(R.drawable.philippines);
         philippines1.setImageResource(R.drawable.philippines1);
+        flag.setImageResource(R.drawable.flag);
+        flag1.setImageResource(R.drawable.flag1);
+        coat.setImageResource(R.drawable.coat);
+    }
+
+    public void btn_back(View view) {
+        startActivity(new Intent(getApplicationContext(),Elementary.class));
     }
 }
